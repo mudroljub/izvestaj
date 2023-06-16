e08a019adec032677d9b908bca839fc55da3fcc9
Damjan Pavlica <mudroljub@gmail.com>
2023-06-16 17:37:14 +0200

    m

--- src/Izvestaj.js
+++ src/Izvestaj.js
@@ -1,35 +1,28 @@
-var brojacTeksta = 0;
-var brojacPotpisa = 0;
-var kuckanjeInterval;
+const elementTekst = document.querySelector('#tekst')
 
-const podaci = {
-  "tekst": "Po mišljenju komandanta Srbije, a u suprotnosti sa iznetim za Hrvatsku, nemiri se u prvom redu i gotovo isključivo imaju pripisati komunističkom uticaju.",
-  "potpis": "Nemački izveštaj od 11. avgusta 1941."
-}
+let i = 0
+let intervalId
+
+const tekst = "Po mišljenju komandanta Srbije, a u suprotnosti sa iznetim za Hrvatsku, nemiri se u prvom redu i gotovo isključivo imaju pripisati komunističkom uticaju.\n\nNemački izveštaj od 11. avgusta 1941."
 
 export class Izvestaj {
   constructor() {
-    this.zvuk = new Audio('kuckanje.mp3');
+    this.zvuk = new Audio('kuckanje.mp3')
     this.start()
   }
 
   start() {
-    this.elementTekst = document.querySelector('#tekst')
-    this.elementPotpis = document.querySelector('#potpis')
-    kuckanjeInterval = window.setInterval(this.kucaj.bind(this), 80);
-    this.zvuk.play();
+    intervalId = window.setInterval(this.kucaj.bind(this), 80)
+    this.zvuk.play()
   }
 
   kucaj() {
-    this.elementTekst.innerHTML += podaci.tekst.charAt(brojacTeksta);
-    brojacTeksta++;
-    if (brojacTeksta > podaci.tekst.length) {
-      this.elementPotpis.innerHTML += podaci.potpis.charAt(brojacPotpisa);
-      brojacPotpisa++;
-    }
-    if (brojacPotpisa >= podaci.potpis.length) {
-      this.zvuk.pause();
-      clearInterval(kuckanjeInterval);
+    elementTekst.innerHTML += tekst.charAt(i)
+    i++
+    elementTekst.innerHTML = elementTekst.innerHTML.replace(/\n/g, '<br>')
+    if (i > tekst.length) {
+      this.zvuk.pause()
+      clearInterval(intervalId)
     }
   }
 }
